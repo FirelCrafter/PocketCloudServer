64fff3b7b743e48100c86b413f22a9bef5fb7357
FireCrafter <firecrafter62@gmail.com>
2023-04-13 14:44:56 +0500

    upload directory fix

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
+import os
 from Application import create_app
+from Application.config import Config
 
 
 app = create_app()
 
 if __name__ == "__main__":
+    if not os.path.exists(Config.UPLOAD_FOLDER):
+        os.makedirs(Config.UPLOAD_FOLDER)
     app.run(debug=True, host='172.24.1.1', port='5000')
